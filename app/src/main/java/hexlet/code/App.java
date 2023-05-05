41923647977a84af8cd920fdff04e1f035cbc3fc
RashidMur <rashidm1990@mail.ru>
2023-05-05 13:04:44 +0300

    add GCD and GCD in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Gcd;
 
 import java.util.Scanner;
 
@@ -13,6 +14,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
@@ -30,6 +32,9 @@ public class App {
                 break;
             case 3 :
                 Calc.calc();
+                break;
+            case 4 :
+                Gcd.gcd();
                 break;
             case 0 :
             default :
---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,47 @@
+package hexlet.code.games;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Gcd {
+    public static void gcd() {
+        Scanner scannerGcdName = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userNameGcd = scannerGcdName.next();
+        System.out.println("Hello, " + userNameGcd + "!");
+        System.out.println("Find the greatest common divisor of given numbers.");
+        int min = 1;
+        int max = 100;
+        int diff = max - min;
+        Scanner scannerGcdAnsver = new Scanner(System.in);
+        Random random = new Random();
+        int i;
+        for (i = 0; i < 3; i++) {
+            int randomNumber = random.nextInt(diff + 1);
+            int randomNumberNext = random.nextInt(diff + 1);
+            randomNumber += min;
+            randomNumberNext += min;
+            System.out.println("Question: " + randomNumber + " " + randomNumberNext);
+            int correctGcd = 1;
+            for (int j = 1; j <= randomNumber && j <= randomNumberNext; j++) {
+                if (randomNumber % j == 0 && randomNumberNext % j == 0) {
+                    correctGcd = j;
+                }
+            }
+            System.out.print("Your choice: ");
+            int gcd = scannerGcdAnsver.nextInt();
+            if (correctGcd == gcd) { // если число correctGcd равно числу gcd то
+                System.out.println("Correct!");
+            } else {
+                System.out.println(gcd + " is wrong answer ;(. Correct answer was " + correctGcd);
+                System.out.println("Let's try again, " + userNameGcd + "!");
+                break;
+            }
+        }
+        if (i == 3) { // если счетчик цикла for становится равен 3 то
+            System.out.println("Congratulations, " + userNameGcd + "!");
+        }
+    }
+}
